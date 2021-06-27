a3607158e316875c03c05145c5cf96a8556aecca
Swaroop <swaroop.chakraborty@cerner.com>
2021-06-27 06:52:18 -0400

    commit

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 import BlogCard from "./BlogCard";
-import Typography from '@material-ui/core/Typography';
-import "./Blogs.css"
+import "./Blogs.css";
 
 function Blogs({ data }) {
     if (data) {
         var blogURL = data.mainURL;
-        var blogClass = data.className
+        var blogClass = data.className;
         var blogs = data.blogs.map(function (blog) {
             return (
                 <div key={blog.id} className="columns portfolio-item">
@@ -16,24 +15,27 @@ function Blogs({ data }) {
         });
     }
 
-
     return (
-
         <section id="blogs">
             <div className="text-container">
                 <div className="row">
                     <br />
                     <br />
-                    <p className="blog-header-short"><i className="fa fa-pencil"></i> Check out few of my blogs here...</p>
+                    <p className="blog-header-short">
+                        <i className="fa fa-pencil"></i> Check out few of my blogs here...
+                    </p>
                     <div className="bgrid-quarters s-bgrid-thirds cf blog-wrapper">
                         {blogs}
                     </div>
                     <br />
-                    <p className="blog-footer"><i className={blogClass}></i> To read more of my blogs visit my Blogger <a href={blogURL}>profile</a></p>
+                    <p className="blog-footer">
+                        <i className={blogClass}></i> To read more of my blogs visit my
+                        Blogger <a href={blogURL}>profile</a>
+                    </p>
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
-export default Blogs
+export default Blogs;
