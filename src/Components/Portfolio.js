afad222fc3682358409a4b88c836b94b6a32e97b
Swaroop <swaroop.chakraborty@cerner.com>
2021-08-09 07:15:11 -0400

    commit

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -10,7 +10,7 @@ const Portfolio = ({ data }) => {
         <div key={projects.title} className="columns portfolio-item">
           <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
-              <img alt={projects.title} src={projectImage} />
+              <img alt={projects.title} src={projectImage}  className="project-thumbnail"/>
               <div className="overlay">
                 <div className="portfolio-item-meta">
                   <h5>{projects.title}</h5>
